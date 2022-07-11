e22ee9db69a79f47eb77a3921a24648cc6ca7b2b
Aleksandr Kozhushkevich <kazhuha@gmail.com>
2022-07-11 13:51:34 +0300

    add user model. User model add to admin panel

--- backend/receptorium/user/models.py
+++ backend/receptorium/user/models.py
@@ -1,3 +1,8 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    password = models.CharField(max_length=150)
+
+    REQUIRED_FIELDS = ['email', 'first_name', 'last_name']
