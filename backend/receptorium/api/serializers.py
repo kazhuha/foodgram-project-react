f5b142670aa80f27a314b95407deade4bb110aaa
Aleksandr Kozhushkevich <kazhuha@gmail.com>
2022-08-02 11:51:24 +0300

    SubscriptionsSerializer, get_recipe changed

--- backend/receptorium/api/serializers.py
+++ backend/receptorium/api/serializers.py
@@ -196,7 +196,7 @@ class SubscriptionsSerializer(CustomUserSerializer):
     def get_recipes(self, obj):
         request = self.context.get('request')
         limit = request.GET.get('recipes_limit')
-        queryset = Recipe.objects.filter(author=obj.author)
+        queryset = Recipe.objects.filter(author=obj)
         if limit:
             queryset = queryset[:int(limit)]
         return RecipeForFavoriteSubscriptionsSerializer(queryset, many=True).data
